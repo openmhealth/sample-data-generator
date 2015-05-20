758a003411cea5dbf349d39edcc41ee70c22d106
Emerson Farrugia <emerson@openmhealth.org>
2015-05-20 21:47:07 +0200

    Update main class to use refactored generators

--- backend/src/main/java/org/openmhealth/data/generator/Application.java
+++ backend/src/main/java/org/openmhealth/data/generator/Application.java
@@ -14,25 +14,28 @@
  * limitations under the License.
  */
 
-package org.openmhealth.data.generator.configuration;
+package org.openmhealth.data.generator;
 
+import com.google.common.collect.Iterables;
 import org.openmhealth.data.generator.domain.MeasureGenerationRequest;
 import org.openmhealth.data.generator.domain.MeasureGroup;
 import org.openmhealth.data.generator.domain.RealValueRandomVariable;
 import org.openmhealth.data.generator.domain.RealValueRandomVariableTrend;
-import org.openmhealth.data.generator.service.*;
-import org.openmhealth.schema.pojos.DataPoint;
+import org.openmhealth.data.generator.service.DataPointGenerationService;
+import org.openmhealth.data.generator.service.DataPointWritingService;
+import org.openmhealth.data.generator.service.MeasureGenerationService;
+import org.openmhealth.schema.domain.omh.BloodPressure;
+import org.openmhealth.schema.domain.omh.BodyWeight;
+import org.openmhealth.schema.domain.omh.DataPoint;
+import org.openmhealth.schema.domain.omh.StepCount;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 import java.io.IOException;
 import java.time.Duration;
@@ -44,12 +47,7 @@ import java.util.List;
 /**
  * @author Emerson Farrugia
  */
-@Configuration
-@PropertySource("classpath:application.yaml")
-@ComponentScan(
-        basePackages = "org.openmhealth",
-        excludeFilters = {@ComponentScan.Filter(value = EnableAutoConfiguration.class)})
-@EnableAutoConfiguration
+@SpringBootApplication
 @EnableConfigurationProperties
 public class Application {
 
@@ -59,20 +57,17 @@ public class Application {
     private MeasureGenerationService measureGenerationService;
 
     @Autowired
-    private BloodPressureDataPointGenerationServiceImpl bloodPressureDataPointGenerationService;
+    private DataPointGenerationService<BloodPressure> bloodPressureDataPointGenerationService;
 
     @Autowired
-    private HeartRateDataPointGenerationServiceImpl heartRateDataPointGenerationService;
+    private DataPointGenerationService<BodyWeight> bodyWeightDataPointGenerationService;
 
     @Autowired
-    private BodyWeightDataPointGenerationServiceImpl bodyWeightDataPointGenerationService;
+    private DataPointGenerationService<StepCount> stepCountDataPointGenerationService;
 
     @Autowired
-    private StepCountDataPointGenerationServiceImpl stepCountDataPointGenerationService;
-
-    @Autowired
-    //    @Qualifier("consoleDataPointWritingServiceImpl")
-    @Qualifier("fileSystemDataPointWritingServiceImpl")
+    @Qualifier("consoleDataPointWritingServiceImpl")
+    // @Qualifier("fileSystemDataPointWritingServiceImpl")
     private DataPointWritingService dataPointWritingService;
 
 
@@ -85,31 +80,33 @@ public class Application {
     }
 
     static {
-        System.setProperty("data.owner", "emerson@openmhealth.org_43");
-        System.setProperty("filename", "step-data.json");
+        System.setProperty("data.userId", "joe");
+        System.setProperty("outputFilename", "step-data.json");
     }
 
     public void run() throws IOException {
 
-        OffsetDateTime startDateTime = OffsetDateTime.now().minusYears(1);
+        OffsetDateTime startDateTime = OffsetDateTime.now().minusDays(3);
         OffsetDateTime endDateTime = OffsetDateTime.now().minusDays(1);
 
         List<DataPoint> dataPoints = new ArrayList<>();
 
         // uncomment as needed
-//        addAll(dataPoints, newBloodPressureDataPoints(startDateTime, endDateTime, 110, 110, 70, 70));
-//        addAll(dataPoints, newBodyWeightDataPoints(startDateTime, endDateTime, 55, 60));
-//        addAll(dataPoints, newHeartRateDataPoints(startDateTime, endDateTime, 80, 80));
-//        addAll(dataPoints, newStepCountPoints(startDateTime, endDateTime, 120, 120, 90, 90));
+        Iterables.addAll(dataPoints, newBloodPressureDataPoints(startDateTime, endDateTime, 110, 110, 70, 70));
+        //              Iterables.addAll(dataPoints, newBodyWeightDataPoints(startDateTime, endDateTime, 55, 60));
+        //              Iterables.addAll(dataPoints, newStepCountDataPoints(startDateTime, endDateTime, 120, 120, 90,
+        // 90));
 
         dataPointWritingService.writeDataPoints(dataPoints);
 
         log.info("A total of {} data point(s) have been generated.", dataPoints.size());
     }
 
-    public Iterable<DataPoint> newBloodPressureDataPoints(OffsetDateTime startDateTime, OffsetDateTime endDateTime,
+    public Iterable<DataPoint<BloodPressure>> newBloodPressureDataPoints(OffsetDateTime startDateTime,
+            OffsetDateTime endDateTime,
             double systolicStart, double systolicEnd, double diastolicStart, double diastolicEnd) {
 
+        // TODO use limits that reflect parameters
         RealValueRandomVariable systolicRandomVariable = new RealValueRandomVariable(3, 100d, 140d);
         RealValueRandomVariable diastolicRandomVariable = new RealValueRandomVariable(3, 60d, 80d);
 
@@ -130,7 +127,8 @@ public class Application {
         return bloodPressureDataPointGenerationService.generateDataPoints(measureGroups);
     }
 
-    public Iterable<DataPoint> newBodyWeightDataPoints(OffsetDateTime startDateTime, OffsetDateTime endDateTime,
+    public Iterable<DataPoint<BodyWeight>> newBodyWeightDataPoints(OffsetDateTime startDateTime,
+            OffsetDateTime endDateTime,
             double weightStart, double weightEnd) {
 
         double minimum = Math.min(weightStart, weightEnd) - 5;
@@ -151,33 +149,16 @@ public class Application {
         return bodyWeightDataPointGenerationService.generateDataPoints(measureGroups);
     }
 
-    public Iterable<DataPoint> newHeartRateDataPoints(OffsetDateTime startDateTime, OffsetDateTime endDateTime,
-            double heartRateStart, double heartRateEnd) {
-
-        RealValueRandomVariable heartRateRandomVariable = new RealValueRandomVariable(5, 50, 200);
-
-        RealValueRandomVariableTrend heartRateTrend =
-                new RealValueRandomVariableTrend(heartRateRandomVariable, heartRateStart, heartRateEnd);
-
-        MeasureGenerationRequest request = new MeasureGenerationRequest();
-        request.setStartDateTime(startDateTime);
-        request.setEndDateTime(endDateTime);
-        request.setMeanInterPointDuration(Duration.ofHours(12));
-        request.addMeasureValueTrend("rate", heartRateTrend);
-
-        Iterable<MeasureGroup> measureGroups = measureGenerationService.generateMeasureGroups(request);
-
-        return heartRateDataPointGenerationService.generateDataPoints(measureGroups);
-    }
-
-    public Iterable<DataPoint> newStepCountPoints(OffsetDateTime startDateTime, OffsetDateTime endDateTime,
+    public Iterable<DataPoint<StepCount>> newStepCountDataPoints(OffsetDateTime startDateTime,
+            OffsetDateTime endDateTime,
             double durationStart, double durationEnd, double stepsPerMinStart, double stepsPerMinEnd) {
 
-        RealValueRandomVariable durationRandomVariable = new RealValueRandomVariable(100, 10, 1800);
+        // TODO use limits that reflect parameters
+        RealValueRandomVariable durationInSecRandomVariable = new RealValueRandomVariable(100, 10, 1800);
         RealValueRandomVariable stepsPerMinRandomVariable = new RealValueRandomVariable(15, 30, 125);
 
-        RealValueRandomVariableTrend durationTrend =
-                new RealValueRandomVariableTrend(durationRandomVariable, durationStart, durationEnd);
+        RealValueRandomVariableTrend durationInSecTrend =
+                new RealValueRandomVariableTrend(durationInSecRandomVariable, durationStart, durationEnd);
         RealValueRandomVariableTrend stepsPerMinTrend =
                 new RealValueRandomVariableTrend(stepsPerMinRandomVariable, stepsPerMinStart, stepsPerMinEnd);
 
@@ -185,11 +166,13 @@ public class Application {
         request.setStartDateTime(startDateTime);
         request.setEndDateTime(endDateTime);
         request.setMeanInterPointDuration(Duration.ofMinutes(30));
-        request.addMeasureValueTrend("duration", durationTrend);
+        request.addMeasureValueTrend("durationInSec", durationInSecTrend);
         request.addMeasureValueTrend("stepsPerMin", stepsPerMinTrend);
 
         Iterable<MeasureGroup> measureGroups = measureGenerationService.generateMeasureGroups(request);
 
         return stepCountDataPointGenerationService.generateDataPoints(measureGroups);
     }
+
+    // TODO add support for heart rate and physical activity
 }
