f0c22b54afec3011cb03f9105b263177ff1b1d8f
Emerson Farrugia <emerson@openmhealth.org>
2015-07-07 12:30:05 +0200

    Reuse Jackson configuration from schema SDK

--- backend/src/main/java/org/openmhealth/data/generator/configuration/JacksonConfiguration.java
+++ backend/src/main/java/org/openmhealth/data/generator/configuration/JacksonConfiguration.java
@@ -16,17 +16,9 @@
 
 package org.openmhealth.data.generator.configuration;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
-import com.fasterxml.jackson.datatype.jsr310.JSR310Module;
-import org.openmhealth.schema.serializer.Rfc3339OffsetDateTimeSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.time.OffsetDateTime;
 
 
 /**
@@ -35,27 +27,11 @@ import java.time.OffsetDateTime;
 @Configuration
 public class JacksonConfiguration {
 
+    /**
+     * @return an {@link ObjectMapper} that matches schema conventions
+     */
     @Bean
     public ObjectMapper objectMapper() {
-
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        // we represent JSON numbers as Java BigDecimals
-        objectMapper.enable(DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS);
-
-        // we serialize dates, date times, and times as strings, not numbers
-        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-
-        // we default to the ISO8601 format for JSR-310 and support Optional
-        objectMapper.registerModule(new JSR310Module());
-        objectMapper.registerModule(new Jdk8Module());
-
-        // but we have to explicitly support the RFC3339 format over ISO8601 to make JSON Schema happy, specifically to
-        // prevent the truncation of zero second fields
-        SimpleModule rfc3339Module = new SimpleModule("rfc3339Module");
-        rfc3339Module.addSerializer(new Rfc3339OffsetDateTimeSerializer(OffsetDateTime.class));
-        objectMapper.registerModule(rfc3339Module);
-
-        return objectMapper;
+        return org.openmhealth.schema.configuration.JacksonConfiguration.newObjectMapper();
     }
 }
